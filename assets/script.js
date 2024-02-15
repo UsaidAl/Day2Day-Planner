bdef6d348bfb30b0ab6b391208bd7026be3d8a82
UsaidAl <ali_usaid98@hotmail.com>
2024-02-15 22:00:34 +0000

    Timeblock Variables

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
 $(function () {
- 
+  $(".saveBtn").on("click", function() {
+    // Get the parent time-block element
+    var timeBlock = $(this).closest(".time-block");
+    // Get the id of the time-block
+    var timeBlockID = timeBlock.attr("id");
+    // Get the user input from the textarea within the time-block
+    var userInput = timeBlock.find("textarea").val();
+    // Save the user input in local storage using the time-block id as the key
+    localStorage.setItem(timeBlockID, userInput);
+  });
+
+
+  
